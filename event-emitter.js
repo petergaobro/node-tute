3b23d9da9e03ef926216065b608445f749060c98
PengGao1027 <37321872+petergaobro@users.noreply.github.com>
2024-07-19 13:29:19 +1000

    event emitter api review

---
+++ event-emitter.js
@@ -0,0 +1,16 @@
+//It is used for event-driven programming. 
+//It provides an event emitter class, on which event listeners can be registered, 
+//and these listeners are triggered when specific events occur.
+// const EventEmitter = require('events');
+
+// const customEmitter = new EventEmitter()
+
+// customEmitter.on('response', (name, id) => {
+//     console.log(`data received user ${name} with id: ${id}`);
+// })
+
+// customEmitter.on('response', () => {
+//     console.log(`some other logic here`);
+// })
+
+// customEmitter.emit('response', 'john', 34)
---
+++ request-event.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+// using event emitter API
+const server = http.createServer()
+
+//emit request event
+//subscribe to it / listen for it / respond to it
+server.on('request', (req, res) => {
+    res.end('Welcome')
+})
+
+server.listen(5000)
