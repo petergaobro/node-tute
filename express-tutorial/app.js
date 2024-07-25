31d397c5abd7d667128530f5bfffb45529f9f793
petergaobro <37321872+petergaobro@users.noreply.github.com>
2024-07-25 12:01:52 +1000

    multiple Middleware function

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express();
 const logger = require('./logger')
-
+const authorize = require('./authorize')
+const morgan = require('morgan')
 // req => middleware => res
-app.use('/api', logger)
+// app.use('/api', logger)
+
+
+// USE VS ROUTE
+// options - our own / express / third party
+
+// app.use([logger, authorize])
+
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
-
     res.send('Home')
 })
 
@@ -18,7 +27,9 @@ app.get('/api/products', (req, res) => {
     res.send('Products')
 })
 
+// app.get('/api/items', [logger, authorize], (req, res) => {
 app.get('/api/items', (req, res) => {
+    console.log(req.user);
     res.send('Items')
 })
 
